e9e650979e6c77b4fede60edeba424773d416bc2
kyeong-hyeok <minkh4227@naver.com>
2024-06-18 11:23:19 +0900

    feat: 딜러의 카드 합이 21초과이면 플레이어가 승리한다.

--- src/test/java/blackjack/WinningTest.java
+++ src/test/java/blackjack/WinningTest.java
@@ -25,4 +25,21 @@ class WinningTest {
         assertThat(dealer.getWinCount()).isEqualTo(1);
         assertThat(player.getWinStatus()).isFalse();
     }
+
+    @Test
+    @DisplayName("딜러의 카드 합이 21초과이면 플레이어가 승리한다.")
+    void player_win() {
+        Dealer dealer = new Dealer(List.of (
+                new Card("5", CardPattern.CLOVER),
+                new Card("J", CardPattern.HEART),
+                new Card("10", CardPattern.HEART)
+        ));
+        Player player = new Player(java.util.List.of (
+                new Card("1", CardPattern.CLOVER),
+                new Card("2", CardPattern.HEART)
+        ), "jackson");
+        Winning.checkWinner(dealer, player);
+        assertThat(dealer.getWinCount()).isEqualTo(0);
+        assertThat(player.getWinStatus()).isTrue();
+    }
 }
