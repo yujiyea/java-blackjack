64c90035554b071827ee698a6ea6f0805f4bd5ca
kyeong-hyeok <minkh4227@naver.com>
2024-06-18 11:34:23 +0900

    feat: 카드 번호는 정해진 수가 아니면 Exception이 발생한다.

--- src/test/java/blackjack/CardTest.java
+++ src/test/java/blackjack/CardTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class CardTest {
     @Test
@@ -16,4 +17,10 @@ class CardTest {
         assertThat(card.getNumber()).isEqualTo("3");
     }
 
+    @Test
+    @DisplayName("카드 번호는 정해진 수가 아니면 Exception이 발생한다.")
+    void card_number_error() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> CardNumber.from("1"));
+    }
 }
